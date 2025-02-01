a4a74b97a548fec699e21b4896415ee75b2f56d3
puntoTres <smr2.1415.alonsoa@gmail.com>
2025-02-01 18:33:47 +0100

    feat(layout): create new structure

---
+++ layouts/default.vue
@@ -0,0 +1,27 @@
+<template>
+  <section class="layout">
+    <header class="layout__header">
+      <UiTheNavbar />
+    </header>
+    <main class="layout__main">
+      <UiTheAside />
+      <slot />
+    </main>
+  </section>
+</template>
+<style lang="scss" scoped>
+.layout {
+  @include flex(column);
+
+  &__header {
+    background-color: aquamarine;
+    width: 100%;
+  }
+
+  &__main {
+    @include flex($align: flex-start, $justify: flex-start);
+
+    width: 100%;
+  }
+}
+</style>
