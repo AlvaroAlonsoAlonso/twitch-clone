86087a94b716af1324cd641eddf716db8c5491d3
puntoTres <smr2.1415.alonsoa@gmail.com>
2025-02-01 18:32:42 +0100

    feat(ui): create component aside and navbar

---
+++ components/ui/TheNavbar.vue
@@ -0,0 +1,15 @@
+<template>
+  <nav class="navbar">
+    <section>Botones izquierdos</section>
+    <h1>MENU TWITCH - buscador</h1>
+    <section>Botones derechos</section>
+  </nav>
+</template>
+<style lang="scss" scoped>
+.navbar {
+  @include flex($justify: space-between);
+
+  color: black;
+  padding: 1em 2em;
+}
+</style>
