93a159621f5193b29d6f7a97d30e1a94cea12bae
alvaro <smr2.1415.alonsoa@gmail.com>
2025-02-02 22:43:13 +0100

    feat(ui): create new component and change with

---
+++ components/ui/TheTab.vue
@@ -0,0 +1,13 @@
+<template>
+  <section class="tab">
+    <p>Español</p>
+  </section>
+</template>
+
+<style lang="scss" scoped>
+.tab {
+  background-color: var(--c-grey-light);
+  padding: 0.4em 1em;
+  border-radius: 1em;
+}
+</style>
