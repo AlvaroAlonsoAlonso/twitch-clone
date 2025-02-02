7a4873eb930a1b69f6bc4c9e6e8e64d5e8459daf
alvaro <smr2.1415.alonsoa@gmail.com>
2025-02-02 21:08:07 +0100

    feat(ui): create new component aside

--- components/ui/TheAside.vue
+++ components/ui/TheAside.vue
@@ -1,32 +1,35 @@
 <template>
   <aside class="aside">
-    <h2>DIRECTOS RECOMENTADOS ASIDE</h2>
-    <ul>
-      <li>Directo 1</li>
-      <li>Directo 2</li>
-      <li>Directo 3</li>
-    </ul>
+    <header class="aside__header">
+      <h3>Canales en directo</h3>
+      <Icon class="aside__icon" name="ix:arrow-left" />
+    </header>
+
+    <UiTheDirectChannel />
+    <UiTheDirectChannel />
+    <UiTheDirectChannel />
+    <UiTheDirectChannel />
   </aside>
 </template>
 <style lang="scss" scoped>
 .aside {
-  background-color: #cd3b3b;
-  color: white;
+  background-color: var(--c-black);
+  color: var(--c-white);
   padding: 1em;
   height: 100vh;
 
-  h2 {
-    font-size: 1.5em;
-    margin-bottom: 1em;
+  &__header {
+    @include flex($gap: 1em);
   }
 
-  ul {
-    list-style: none;
-    padding: 0;
+  h3 {
+    font-size: 1.5em;
+  }
 
-    li {
-      margin-bottom: 0.5em;
-    }
+  &__icon {
+    font-size: 1em;
+    padding: 1em;
+    cursor: pointer;
   }
 }
 </style>
