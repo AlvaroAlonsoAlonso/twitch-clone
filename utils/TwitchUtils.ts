fc019a92ced2eef97c8b4dffba26b2557e878b21
alvaro <smr2.1415.alonsoa@gmail.com>
2025-02-05 00:25:44 +0100

    feat(utils): create class utils

---
+++ utils/TwitchUtils.ts
@@ -0,0 +1,31 @@
+export default class TwitchUtils {
+  formatThumbnail(
+    thumbnailUrl: string,
+    width: number = 70,
+    height: number = 70,
+  ): string {
+    if (!thumbnailUrl) return ''
+    return thumbnailUrl
+      .replace('{width}', width.toString())
+      .replace('{height}', height.toString())
+  }
+
+  formatthumbnailUrl(thumbnailUrl: string): string {
+    return this.formatThumbnail(thumbnailUrl, 802, 452)
+  }
+
+  formatTime(isoDate: string): string {
+    const diffMs = Date.now() - new Date(isoDate).getTime()
+    const hours = Math.floor(diffMs / 3600000)
+      .toString()
+      .padStart(2, '0')
+    const minutes = Math.floor((diffMs % 3600000) / 60000)
+      .toString()
+      .padStart(2, '0')
+    const seconds = Math.floor((diffMs % 60000) / 1000)
+      .toString()
+      .padStart(2, '0')
+
+    return `${hours}:${minutes}:${seconds}`
+  }
+}
